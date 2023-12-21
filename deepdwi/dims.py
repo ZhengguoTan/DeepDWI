961e4ccb3d6a9fe9c63d6575eef937397453014e
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-12-21 10:43:23 +0100

    update dims: add DIM_REP for repetition

--- deepdwi/dims.py
+++ deepdwi/dims.py
@@ -2,9 +2,10 @@
 Generalized data dimensions for multi-purpose MRI image reconstruction.
 """
 
-DIM_X = -1
-DIM_Y = -2
-DIM_Z = -3
-DIM_COIL = -4
+DIM_X    = -1  # readout
+DIM_Y    = -2  # phase-encoding
+DIM_Z    = -3  # slice
+DIM_COIL = -4  # coil
 DIM_ECHO = -5  # For diffusion MRI, it stores the shots per DWI
 DIM_TIME = -6  # For diffusion MRI, it stores the diffusion encodings
+DIM_REP  = -7  # repetition

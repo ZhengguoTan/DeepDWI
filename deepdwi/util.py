10b5d7a17f0e6c4fd54b632daf70df6ef485510b
Zhengguo Tan <zhengguo.tan@gmail.com>
2024-01-04 15:45:49 +0100

    add the function xpay in util

--- deepdwi/util.py
+++ deepdwi/util.py
@@ -18,6 +18,7 @@ def rss(x: Tensor,
 
     return torch.sqrt(torch.sum(abs(x)**2, dim=dim, keepdim=keepdim))
 
+
 def axpy(y: Tensor, a, x: Tensor):
     """Compute y = a * x + y.
 
@@ -28,3 +29,15 @@ def axpy(y: Tensor, a, x: Tensor):
 
     """
     y += a * x
+
+
+def xpay(y: Tensor, a, x: Tensor):
+    """Compute y = x + a * y.
+
+    Args:
+        y (Tensor): Output array.
+        a (scalar or Tensor): Input scalar.
+        x (Tensor): Input array.
+    """
+    y *= a
+    y += x

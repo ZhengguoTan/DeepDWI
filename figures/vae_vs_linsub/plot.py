41328a5f4d268cd8f95918cd047134b216b78a92
Zhengguo Tan <zhengguo.tan@gmail.com>
2024-03-18 21:34:11 +0100

    brain result on vae_vs_linsub

--- figures/vae_vs_linsub/plot.py
+++ figures/vae_vs_linsub/plot.py
@@ -4,8 +4,8 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-from skimage.metrics import structural_similarity as ssim
-from skimage.metrics import normalized_root_mse as nrmse
+# from skimage.metrics import structural_similarity as ssim
+# from skimage.metrics import normalized_root_mse as nrmse
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -29,9 +29,10 @@ DWI_LINSUB_1 = DWI_LINSUB[:, 1, :, :]
 
 row, col = 2, 3
 
+diff_idx = [16]
 
 
-for d in range(N_diff):
+for d in diff_idx:
 
     print('> diff %3d'%(d))
 
@@ -42,40 +43,28 @@ for d in range(N_diff):
     vmax = np.amax(muse)
     ax[0][0].imshow(muse, cmap='gray', interpolation=None,
                     vmin=vmin, vmax=vmax)
+    ax[0][0].text(0.02*N_x, 0.08*N_y, 'MUSE',
+                  fontsize=16, color='w')
+
 
     linsub = abs(DWI_LINSUB_1[d])
     ax[0][1].imshow(linsub, cmap='gray', interpolation=None,
                     vmin=vmin, vmax=vmax)
+    ax[0][1].text(0.02*N_x, 0.08*N_y, 'SVD',
+                  fontsize=16, color='w')
 
-    # ssim_linsub = ssim(muse, linsub,
-    #                    data_range=np.amax(muse) - np.amin(muse))
-    # ax[0][1].text(0.75*N_x, 0.96*N_y, '%5.3f'%ssim_linsub,
-    #           color='w', fontsize=16, weight='bold')
-
-    mse_linsub = nrmse(muse, linsub)
-    ax[0][1].text(0.75*N_x, 0.96*N_y, '%5.3f'%mse_linsub,
-              color='w', fontsize=16, weight='bold')
 
     vae = abs(DWI_VAE_1[d])
     ax[0][2].imshow(vae, cmap='gray', interpolation=None,
                     vmin=vmin, vmax=vmax)
+    ax[0][2].text(0.02*N_x, 0.08*N_y, 'VAE',
+                  fontsize=16, color='w')
 
-    # ssim_vae = ssim(muse, vae,
-    #                 data_range=np.amax(muse) - np.amin(muse))
-    # ax[0][2].text(0.75*N_x, 0.96*N_y, '%5.3f'%ssim_vae,
-    #           color='w', fontsize=16, weight='bold')
-
-    mse_vae = nrmse(muse, vae)
-    ax[0][2].text(0.75*N_x, 0.96*N_y, '%5.3f'%mse_vae,
-              color='w', fontsize=16, weight='bold')
-
-    print('  ', mse_linsub, mse_vae)
-
-    # print('linsub max %8.6f vae max %8.6f'%(np.amax(linsub), np.amax(vae)))
 
     diff_l = muse - linsub
     ax[1][1].imshow(muse - linsub, cmap='gray', interpolation=None,
                     vmin=vmin, vmax=np.amax(diff_l)*0.2)
+    ax[1][1].set_ylabel('diff X 5', fontsize=16)
 
     diff_v = muse - vae
     ax[1][2].imshow(diff_v, cmap='gray', interpolation=None,
@@ -83,9 +72,10 @@ for d in range(N_diff):
 
     for m in range(row):
         for n in range(col):
-            ax[m][n].set_axis_off()
+            ax[m][n].axes.xaxis.set_ticks([])
+            ax[m][n].axes.yaxis.set_ticks([])
 
     plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig(DIR + '/diff_' + str(d).zfill(2) + '.png',
+    plt.savefig(DIR + '/vae_vs_linsub_brain_diff_' + str(d).zfill(2) + '.png',
                 bbox_inches='tight', pad_inches=0, dpi=300)
     plt.close()

1f0d2337a0a21edaf7ca4270a520e0380a296124
Zhengguo Tan <zhengguo.tan@gmail.com>
2025-11-16 15:40:48 +0100

    svg supp figures

--- figures/convert_svg.sh
+++ figures/convert_svg.sh
@@ -30,3 +30,29 @@ for (( N=0; N<${ARR_LEN}; N++ )); do
 
     convert ${PNG_DST}.png ${PNG_DST}.tiff
 done
+
+
+declare -a SVG_SUPP_FILE=(
+    models/resnet                    # 1
+    ablation/ablation                # 2
+    intersubject/intersubject        # 3
+)
+
+ARR_LEN=${#SVG_SUPP_FILE[@]}
+
+for (( N=0; N<${ARR_LEN}; N++ )); do
+
+    echo "  converting Figure S$((N+1))"
+
+    PDF_DST=figS$((N+1))
+    PNG_DST=figS$((N+1))
+
+    # inkscape --version < 1
+    # inkscape --export-pdf=${PDF_DST}.pdf -d 300 ${SVG_MAIN_FILE[$N]}.svg
+    # inkscape --export-png=${PNG_DST}.png -d 300 ${SVG_MAIN_FILE[$N]}.svg
+
+    # inkscape --version > 1
+    inkscape --export-type="png" --export-filename=${PNG_DST} -d 300 ${SVG_SUPP_FILE[$N]}.svg
+
+    convert ${PNG_DST}.png ${PNG_DST}.tiff
+done

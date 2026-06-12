5ff3504045627bcc750bbbe5880d7f0a73bb53d4
Zhengguo Tan <zhengguo.tan@gmail.com>
2026-06-12 10:38:52 -0400

    combine one single slice

--- examples/comb_slice.py
+++ examples/comb_slice.py
@@ -12,7 +12,7 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 # %% options
 parser = argparse.ArgumentParser(description='Read in slice files, append them, and save in correct order.')
 
-parser.add_argument('--dir', default='2024-05-13_zsssl_0.7mm_21-dir_R2x2_vol2_scan2_kdat_slice_040_norm-kdat-1.0_navi_ResNet2D_ResBlock-12_kernel-3_ADMM_08_lamda-0.050_Adam_lr-0.000500_MixL1L2Loss',
+parser.add_argument('--data_dir', default='2024-05-13_zsssl_0.7mm_21-dir_R2x2_vol2_scan2_kdat_slice_040_norm-kdat-1.0_navi_ResNet2D_ResBlock-12_kernel-3_ADMM_08_lamda-0.050_Adam_lr-0.000500_MixL1L2Loss',
                     help='directory in which the data are read.')
 
 parser.add_argument('--method_pre', default='zsssl',
@@ -30,8 +30,8 @@ parser.add_argument('--MB', type=int, default=2,
 parser.add_argument('--slices', type=int, default=88,
                     help='total number of slices.')
 
-parser.add_argument('--slice_idx', type=int, default=0,
-                    help='slice index to start with.')
+parser.add_argument('--slice_idx', type=int, default=-1,
+                    help='recon only one slice given its index [default: -1].')
 
 parser.add_argument('--order', type=str,
                     choices=['interleaved', 'sequential'],
@@ -50,21 +50,26 @@ parser.add_argument('--resize', action='store_true',
 parser.add_argument('--zf', type=int, default=12,
                     help='zero-fill in the kx and ky dimensions.')
 
+parser.add_argument('--flip_axes', nargs='+', type=int,
+                    default=None)
+
 args = parser.parse_args()
 
-# %%
-acq_slice = []
-
-if args.slices == 1:
-    slice_loop = range(args.slice_idx, args.slice_idx + 1)
+if args.slice_idx >= 0:
+    slice_loop = range(args.slice_idx, args.slice_idx + 1, 1)
 else:
     slice_loop = range(args.slices)
+
+# %%
+DATA_DIR = DIR + '/' + args.data_dir
+
+acq_slice = []
 
 for s in slice_loop:
 
     print('> slice ' + str(s).zfill(3))
 
-    fstr = DIR + '/' + args.dir + '/' + args.method_pre + '_slice_' + str(s).zfill(3) + args.method_post
+    fstr = DATA_DIR + '/' + args.method_pre + '_slice_' + str(s).zfill(3) + args.method_post
 
     with h5py.File(fstr + '.h5', 'r') as f:
         I = f[args.key][:]
@@ -94,16 +99,16 @@ elif args.order == 'sequential':
 
 reo_slice = np.squeeze(reo_slice)
 
-if args.slices == 1:
+if reo_slice.ndim == 3:
     reo_slice = reo_slice[:, None, :, :]
 
-print('> reo_slice shape: ', reo_slice.shape)
+print('- squeeze --> ', reo_slice.shape)
 
 if args.resize is True:
     N_diff, N_z, N_y, N_x = reo_slice.shape
     N_i = min(N_y, N_x)
     reo_slice = sp.resize(reo_slice, [N_diff, N_z, N_i, N_i])
-    print('> reo_slice shape: ', reo_slice.shape)
+    print('- resize --> ', reo_slice.shape)
 
 N_y, N_x = reo_slice.shape[-2:]
 
@@ -111,8 +116,14 @@ if args.zf > N_y and args.zf > N_x:
     reo_slice = sp.fft(reo_slice, axes=(-2, -1))
     reo_slice = sp.resize(reo_slice, list(reo_slice.shape[:-2]) + [args.zf, args.zf])
     reo_slice = sp.ifft(reo_slice, axes=(-2, -1))
-    print('> reo_slice shape: ', reo_slice.shape)
+    print('- zf --> ', reo_slice.shape)
 
-f = h5py.File(DIR + '/' + args.dir + '/' + args.method_pre + args.method_post + '.h5', 'w')
+if args.flip_axes is not None:
+    print('- flip axes: ', args.flip_axes)
+    reo_slice = np.flip(reo_slice, axis=tuple(args.flip_axes))
+
+print('> reo_slice shape: ', reo_slice.shape)
+
+f = h5py.File(DATA_DIR + '/' + args.method_pre + args.method_post + '.h5', 'w')
 f.create_dataset('DWI', data=reo_slice)
 f.close()
